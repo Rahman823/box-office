5d0a92b5874b6c533644d1658668d92d419e82d2
rahman <loveyougit@.com>
2021-06-06 15:45:49 +0530

    Added layout and title 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route } from 'react-router-dom';
 
-
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
-  return <Switch>
-    <Route exact path="/">
-      This is home page
-    </Route>
-    
-    <Route exact path="/starred">
-      This is Starred page
-    </Route>
-    
-    <Route>
-      This is 404 page
-    </Route>
-  </Switch>
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
